42d88c3c24b2bdde2262e6e31a422597f1d03b3b
Erfan Khedmati <100248103+EranKhedmati@users.noreply.github.com>
2025-07-23 01:28:31 +0330

    create routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,27 @@
-import React from 'react'
+import React from "react";
+
+import { Routes, Route } from "react-router-dom";
 
 // Components
-import MovieList from './components/MovieList'
-import SearchBar from './components/SearchBar'
+import MovieList from "./components/MovieList";
+import SearchBar from "./components/SearchBar";
+import Layout from "./Layout/Layout";
+import Navbar from "./Layout/Navbar";
+import Footer from "./Layout/Footer";
+
+// Pages
+import Home from "./pages/Home";
+import Search from "./pages/Search";
 
 function App() {
   return (
-    <div className='w-full h-full bg-black/93 text-white flex flex-col items-center'>
-      <SearchBar />
-      <MovieList />
-    </div>
-  )
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="search" element={<Search />} />
+      </Route>
+    </Routes>
+  );
 }
 
-export default App
+export default App;
