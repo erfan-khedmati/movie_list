fd95313c12e127d7386690ad6e5a391c98fa909a
Erfan Khedmati <100248103+EranKhedmati@users.noreply.github.com>
2025-07-23 01:36:53 +0330

    design the layout

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import { Outlet } from 'react-router-dom'
+
+// Components
+import Navbar from './Navbar'
+import Footer from './Footer'
 
 function Layout() {
   return (
-    <div>Layout</div>
+    <div className='w-screen h-screen'>
+      <Navbar />
+      <main className='w-full'>
+        <Outlet />
+      </main>
+      <Footer />
+    </div>
   )
 }
 
